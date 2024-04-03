b9a27f73c360f6e03610feb968119f6f9fdfd20e
Tamilvanan37 <138431632+Tamilvanan37@users.noreply.github.com>
2024-04-03 19:56:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import StudentForm from './Component/StudentForm';
+import StudentTable from './Component/StudentTable';
+
+function App() {
+  return (
+    <div>
+      <StudentForm />
+      <div style={{marginTop:'2rem'}}>
+        <StudentTable />
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
